3015a84aceb53c59c3c834431d63861977e0559a
Sylvanrj <sylvan.juarez@gmail.com>
2025-06-26 14:09:31 -0400

    Add files via upload

---
+++ pages/admin.tsx
@@ -0,0 +1,9 @@
+
+export default function Admin() {
+  return (
+    <div>
+      <h1>Admin Page</h1>
+      <p>This is where you will manage golf courses.</p>
+    </div>
+  );
+}
---
+++ pages/dashboard.tsx
@@ -0,0 +1,9 @@
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Set filters and scan for tee times here.</p>
+    </div>
+  );
+}
