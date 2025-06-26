776b568af0df36db0b6bcd60434745c1d35360b3
Sylvanrj <sylvan.juarez@gmail.com>
2025-06-26 14:54:22 -0400

    Delete pages directory

--- pages/admin.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Admin() {
-  return (
-    <div>
-      <h1>Admin Page</h1>
-      <p>This is where you will manage golf courses.</p>
-    </div>
-  );
-}
--- pages/dashboard.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Dashboard() {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Set filters and scan for tee times here.</p>
-    </div>
-  );
-}
