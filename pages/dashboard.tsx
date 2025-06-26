5562b2984d93e8c8c7f15bc78a7bcdf6e2cdfe39
Sylvanrj <sylvan.juarez@gmail.com>
2025-06-26 11:48:49 -0400

    Delete pages directory

--- pages/dashboard.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Dashboard() {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Set filters and view tee time matches here.</p>
-    </div>
-  );
-}
--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to Tee Time Scanner</h1>
-      <p>Go to /admin to manage courses or /dashboard to scan tee times.</p>
-    </div>
-  );
-}
