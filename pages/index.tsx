88a4993b613f3931dacb8ea729e13f9f81e845d3
Sylvanrj <sylvan.juarez@gmail.com>
2025-06-26 11:43:38 -0400

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Tee Time Scanner</h1>
+      <p>Go to /admin to manage courses or /dashboard to scan tee times.</p>
+    </div>
+  );
+}
